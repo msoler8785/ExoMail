366614a60bf3664024087e2899138f874ca1d170
msoler8785 <msoler8785@gmail.com>
2016-05-23 21:38:23 -0400

    Update example project.

---
+++ ExoMail.Example/MemoryConfig.cs
@@ -0,0 +1,117 @@
+﻿using ExoMail.Smtp.Interfaces;
+using System;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ExoMail.Example
+{
+    public class MemoryConfig : IServerConfig
+    {
+        public string HostName { get; set; }
+
+        public bool IsAuthRequired { get; set; }
+
+        public bool IsEncryptionRequired { get; set; }
+
+        public bool IsStartTlsSupported { get; set; }
+
+        public bool IsTls { get; set; }
+
+        public int MaxMessageSize { get; set; }
+
+        public int Port { get; set; }
+
+        public string ServerId { get; set; }
+
+        public IPAddress ServerIpBinding { get; set; }
+
+        public int SessionTimeout { get; set; }
+
+        public X509Certificate2 X509Certificate2 { get; set; }
+
+        public MemoryConfig()
+        {
+            HostName = Environment.MachineName;
+            IsAuthRequired = false;
+            IsEncryptionRequired = false;
+            IsStartTlsSupported = false;
+            IsTls = false;
+            MaxMessageSize = int.MaxValue;
+            Port = 25;
+            ServerId = Guid.NewGuid().ToString();
+            ServerIpBinding = IPAddress.Any;
+            SessionTimeout = int.MaxValue;
+        }
+
+        public static MemoryConfig Create()
+        {
+            return new MemoryConfig();
+        }
+
+        public MemoryConfig WithHostname(string hostName)
+        {
+            this.HostName = hostName;
+            return this;
+        }
+
+        public MemoryConfig WithAuthenticationRequired()
+        {
+            this.IsAuthRequired = true;
+            return this;
+        }
+
+        public MemoryConfig WithStartTlsSupported()
+        {
+            this.IsStartTlsSupported = true;
+            return this;
+        }
+
+        public MemoryConfig WithEncryptionRequired()
+        {
+            this.IsEncryptionRequired = true;
+            return this;
+        }
+
+        public MemoryConfig WithTls()
+        {
+            IsEncryptionRequired = true;
+            return this;
+        }
+
+        public MemoryConfig WithMaxMessageSize(int maxMessageSize)
+        {
+            MaxMessageSize = maxMessageSize;
+            return this;
+        }
+
+        public MemoryConfig WithPort(int port)
+        {
+            Port = port;
+            return this;
+        }
+
+        public MemoryConfig WithServerId(string serverId)
+        {
+            ServerId = serverId;
+            return this;
+        }
+
+        public MemoryConfig WithServerIpBinding(IPAddress ipBinding)
+        {
+            ServerIpBinding = ipBinding;
+            return this;
+        }
+
+        public MemoryConfig WithSessionTimeout(int timeout)
+        {
+            SessionTimeout = timeout;
+            return this;
+        }
+
+        public MemoryConfig WithX509Certificate(string path, string password)
+        {
+            X509Certificate2 = new X509Certificate2(path, password);
+            return this;
+        }
+    }
+}
--- ExoMail.Example/Program.cs
+++ ExoMail.Example/Program.cs
@@ -1,20 +1,18 @@
-﻿using ExoMail.Smtp.Interfaces;
+﻿using ExoMail.Smtp.Authentication;
+using ExoMail.Smtp.Interfaces;
+using ExoMail.Smtp.Protocol;
+using ExoMail.Smtp.Server.Authentication;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ExoMail.Smtp.Protocol;
 using System.IO;
-using System.Net;
-using System.Security.Cryptography.X509Certificates;
 using System.Net.Sockets;
+using System.Threading.Tasks;
 
 namespace ExoMail.Example
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             var appStart = new AppStart();
             appStart.Start();
@@ -22,9 +20,11 @@ namespace ExoMail.Example
             Console.ReadLine();
         }
     }
+
     public class AppStart
     {
         public TcpListener TcpListener { get; set; }
+
         public void Start()
         {
             Task.Run(() => StartListeningAsync()).Wait();
@@ -32,9 +32,19 @@ namespace ExoMail.Example
 
         public async Task StartListeningAsync()
         {
-            var config = new MemoryConfig();
+            var config = MemoryConfig.Create()
+                            .WithHostname("exomail01.example.com")
+                            .WithPort(2525)
+                            .WithServerId("FC30BD4D-1C93-4FBF-BF8F-9788059AF0DC")
+                            .WithSessionTimeout(TimeSpan.FromMinutes(5).Milliseconds)
+                            .WithX509Certificate(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "localhost.pfx"), null)
+                            .WithEncryptionRequired()
+                            .WithStartTlsSupported();
+
             var messageStore = new FileMessageStore();
             var userStore = new TestUserStore();
+
+            UserManager.GetUserManager.AddUserStore(userStore);
 
             this.TcpListener = new TcpListener(config.ServerIpBinding, config.Port);
             this.TcpListener.Start();
@@ -57,11 +67,12 @@ namespace ExoMail.Example
                     MessageStore = messageStore,
                     UserStore = userStore
                 };
-
+                session.SaslMechanisms.Add(new LoginSaslMechanism());
                 await session.BeginSession(tcpClient);
             });
         }
     }
+
     public class FileMessageStore : IMessageStore
     {
         public async Task Save(MemoryStream memoryStream, SmtpReceivedHeader receivedHeader)
@@ -80,94 +91,6 @@ namespace ExoMail.Example
 
                 await headers.CopyToAsync(stream);
                 await memoryStream.CopyToAsync(stream);
-            }
-        }
-    }
-
-    public class MemoryConfig : IServerConfig
-    {
-        public string HostName
-        {
-            get { return Environment.MachineName; }
-        }
-
-        public bool IsAuthRequired
-        {
-            get
-            {
-                return false;
-            }
-        }
-
-        public bool IsEncryptionRequired
-        {
-            get
-            {
-                return false;
-            }
-        }
-
-        public bool IsStartTlsSupported
-        {
-            get
-            {
-                return false;
-            }
-        }
-
-        public bool IsTls
-        {
-            get
-            {
-                return false;
-            }
-        }
-
-        public int MaxMessageSize
-        {
-            get
-            {
-                return int.MaxValue;
-            }
-        }
-
-        public int Port
-        {
-            get
-            {
-                return 2525;
-            }
-        }
-
-        public string ServerId
-        {
-            get
-            {
-                return "00001";
-            }
-        }
-
-        public IPAddress ServerIpBinding
-        {
-            get
-            {
-                return IPAddress.Any;
-            }
-        }
-
-        public int SessionTimeout
-        {
-            get
-            {
-                return int.MaxValue;
-            }
-        }
-
-        public X509Certificate2 X509Certificate2
-        {
-            get
-            {
-                return null;
             }
         }
     }
@@ -194,7 +117,7 @@ namespace ExoMail.Example
 
         public bool IsUserAuthenticated(string userName, string password)
         {
-            throw new NotImplementedException();
+            return userName.ToUpper() == "TUSER" && password == "Str0ngP@$$!!";
         }
 
         public bool IsValidRecipient(string emailAddress)
