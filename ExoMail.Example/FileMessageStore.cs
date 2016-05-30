50743bffe75b7c4b99a08371bda93614fcfd2a99
msoler8785 <msoler8785@gmail.com>
2016-05-30 06:51:33 -0400

    Refactor example project

---
+++ ExoMail.Example/FileMessageStore.cs
@@ -0,0 +1,39 @@
+﻿using ExoMail.Smtp.Interfaces;
+using ExoMail.Smtp.Protocol;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExoMail.Example
+{
+    public class FileMessageStore : IMessageStore
+    {
+        public async Task Save(
+            MemoryStream memoryStream, 
+            SmtpReceivedHeader receivedHeader, 
+            IMessageEnvelope messageEnvelope
+            )
+        {
+            var directory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Messages");
+            var path = Path.Combine(directory, Guid.NewGuid().ToString() + ".eml");
+
+            if (!Directory.Exists(directory))
+            {
+                Directory.CreateDirectory(directory);
+            }
+
+            using (var stream = new FileStream(path, FileMode.Create))
+            {
+                var headers = await receivedHeader.GetReceivedHeaders();
+
+                await headers.CopyToAsync(stream);
+                await memoryStream.CopyToAsync(stream);
+            }
+
+            messageEnvelope.SaveEnvelope(path);
+        }
+    }
+}
