83a2533a506494afcdc24185489f85ca0ae176df
msoler8785 <msoler8785@gmail.com>
2016-05-31 23:40:12 -0400

    Add SaslFactory

---
+++ ExoMail.Smtp/Authentication/SaslMechanismFactory.cs
@@ -0,0 +1,27 @@
+﻿using ExoMail.Smtp.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExoMail.Smtp.Authentication
+{
+    public class SaslFactory
+    {
+        public static ISaslMechanism GetSaslMechanism(string mechanism)
+        {
+            mechanism = mechanism.ToUpper();
+
+            switch(mechanism)
+            {
+                case "PLAIN":
+                    return new PlainSaslMechanism();
+                case "LOGIN":
+                    return new LoginSaslMechanism();
+                default:
+                    return null;
+            }
+        }
+    }
+}
