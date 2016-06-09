0b65be167def6e35e0d0134b05e0a6bf45ad8798
msoler8785 <msoler8785@gmail.com>
2016-06-08 23:11:19 -0400

    Add QueueProcessor component project.

---
+++ ExoMail.QueueProcessor/Utilities/RecipientDomainEqualityComparer.cs
@@ -0,0 +1,23 @@
+﻿using ExoMail.Smtp.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExoMail.QueueProcessor.Utilities
+{
+    public class RecipientDomainEqualityComparer : IEqualityComparer<Recipient>
+    {
+        public bool Equals(Recipient x, Recipient y)
+        {
+            return x.RecipientDomain.ToUpper() == y.RecipientDomain.ToUpper();
+        }
+
+        public int GetHashCode(Recipient obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
