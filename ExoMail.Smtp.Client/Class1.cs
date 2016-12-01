5e34d221179558aa9711e2f9bd4b931610e10173
Matt Soler <msoler8785@gmail.com>
2016-11-30 22:31:00 -0500

    Update libs.

---
+++ ExoMail.Smtp.Client/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExoMail.Smtp.Client
+{
+    public class Class1
+    {
+        public Class1()
+        {
+        }
+    }
+}
